b13ac8cc512f1744363ca3752b3bc4e1575dc9ca
DoodleBob <riley.foust18@gmail.com>
2022-07-05 20:13:32 -0400

    added html and basic js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let submitBtn = document.querySelector('#submit')
+
+const convertHandler = function() {
+  let firstUnit = document.querySelector('#funit');
+  let secondUnit = document.querySelector('#sunit');
+  let unit = document.querySelector('#units')
+}
+
+submitBtn.addEventListener('click', convertHandler);
