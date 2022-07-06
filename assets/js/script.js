c6fb8e1f0afda1088e0ac93e2e39a7d537bf96ed
DoodleBob <riley.foust18@gmail.com>
2022-07-05 20:47:19 -0400

    added foot conversions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
 let submitBtn = document.querySelector('#submit')
 
 const convertHandler = function() {
-  let firstUnit = document.querySelector('#funit');
-  let secondUnit = document.querySelector('#sunit');
-  let unit = document.querySelector('#units')
+  let number = document.querySelector('#number');
+  let firstUnit = document.querySelector('#firstUnit')
+  let secondUnit = document.querySelector('#secondUnit')
+  let answer = document.querySelector('#answer')
+
+  if (firstUnit.value == secondUnit.value) {
+    answer.textContent = `${number.value} ${firstUnit.value}`
+  }
+
+  if (firstUnit.value == 'kilometer' && secondUnit.value == 'foot') {
+    answer.textContent = `${+number.value * 3280.84} Feet`
+  }
+
+  if (firstUnit.value == 'meter' && secondUnit.value == 'foot') {
+    answer.textContent = `${+number.value * 3.28084} Feet`
+  }
+
+  if (firstUnit.value == 'centimeter' && secondUnit.value == 'foot') {
+    answer.textContent = `${+number.value * 0.0328084} Feet`
+  }
+
+  if (firstUnit.value == 'millimeter' && secondUnit.value == 'foot') {
+    answer.textContent = `${+number.value * 0.00328084} Feet`
+  }
 }
 
 submitBtn.addEventListener('click', convertHandler);
